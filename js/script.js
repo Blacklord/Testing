8ee87b306ab7eaae4dc923b8aa3b0b5a1cdc8c60
Goran Crnković <goran.crnkovic@gmail.com>
2013-07-11 09:44:18 +0200

    Testing Still

---
+++ js/script.js
@@ -0,0 +1,13 @@
+(function($){
+	// Smooth scroll
+	$('a[href^="#"]').on('click',function (e) {
+		e.preventDefault();
+		var target = this.hash,
+		$target = $(target);
+		$('html, body').stop().animate({
+			    'scrollTop': $target.offset().top
+			}, 500, 'swing', function () {
+		    window.location.hash = target;
+		});
+	});
+})(window.jQuery);
